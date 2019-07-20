7d6aec185c1305620cadb86d068c35251975eed6
tucan9389 <tucan9389@gmail.com>
2019-07-20 21:17:58 +0900

    Upload new project

---
+++ ObjectSegmentation-CoreML/LiveImageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ImageSegmentation-CoreML
+//
+//  Created by Doyoung Gwak on 20/07/2019.
+//  Copyright © 2019 Doyoung Gwak. All rights reserved.
+//
+
+import UIKit
+
+class LiveImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
---
+++ ObjectSegmentation-CoreML/StillImageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StillImageViewController.swift
+//  ObjectSegmentation-CoreML
+//
+//  Created by Doyoung Gwak on 20/07/2019.
+//  Copyright © 2019 Doyoung Gwak. All rights reserved.
+//
+
+import UIKit
+
+class StillImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
