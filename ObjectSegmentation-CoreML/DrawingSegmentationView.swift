3050b5055d8dc287466983f8b0d27d32fa06629d
tucan9389 <tucan9389@gmail.com>
2019-07-20 21:33:51 +0900

    Add video capture on LiveImageViewController

---
+++ ObjectSegmentation-CoreML/DrawingSegmentationView.swift
@@ -0,0 +1,21 @@
+//
+//  DrawingSegmentationView.swift
+//  ObjectSegmentation-CoreML
+//
+//  Created by Doyoung Gwak on 20/07/2019.
+//  Copyright © 2019 Doyoung Gwak. All rights reserved.
+//
+
+import UIKit
+
+class DrawingSegmentationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- ObjectSegmentation-CoreML/LiveImageViewController.swift
+++ ObjectSegmentation-CoreML/LiveImageViewController.swift
@@ -10,9 +10,77 @@ import UIKit
 
 class LiveImageViewController: UIViewController {
 
+    // MARK: - UI Properties
+    @IBOutlet weak var videoPreview: UIView!
+    @IBOutlet weak var drawingView: DrawingSegmentationView!
+    
+    @IBOutlet weak var inferenceLabel: UILabel!
+    @IBOutlet weak var etimeLabel: UILabel!
+    @IBOutlet weak var fpsLabel: UILabel!
+    
+    // MARK: - AV Properties
+    var videoCapture: VideoCapture!
+    
+    // MARK: - View Controller Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // setup camera
+        setUpCamera()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.videoCapture.start()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.videoCapture.stop()
+    }
+    
+    // MARK: - Setup camera
+    func setUpCamera() {
+        videoCapture = VideoCapture()
+        videoCapture.delegate = self
+        videoCapture.fps = 50
+        videoCapture.setUp(sessionPreset: .vga640x480) { success in
+            
+            if success {
+                // UI에 비디오 미리보기 뷰 넣기
+                if let previewLayer = self.videoCapture.previewLayer {
+                    self.videoPreview.layer.addSublayer(previewLayer)
+                    self.resizePreviewLayer()
+                }
+                
+                // 초기설정이 끝나면 라이브 비디오를 시작할 수 있음
+                self.videoCapture.start()
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        resizePreviewLayer()
+    }
+    
+    func resizePreviewLayer() {
+        videoCapture.previewLayer?.frame = videoPreview.bounds
     }
 }
 
+// MARK: - VideoCaptureDelegate
+extension LiveImageViewController: VideoCaptureDelegate {
+    func videoCapture(_ capture: VideoCapture, didCaptureVideoFrame pixelBuffer: CVPixelBuffer?/*, timestamp: CMTime*/) {
+        
+        // the captured image from camera is contained on pixelBuffer
+        if let pixelBuffer = pixelBuffer {
+            // TODO
+        }
+    }
+}
